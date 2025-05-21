0c537ed9ce1fde67d802ca75fc77fff128355cee
Joyan9 <83577461+Joyan9@users.noreply.github.com>
2025-05-21 15:57:24 +0000

    Rough plan for processing script

--- processing/spark_job.py
+++ processing/spark_job.py
@@ -4,8 +4,17 @@ spark = SparkSession.builder.appName("KCC_Pipeline").getOrCreate()
 
 print("Hello World, spark imported")
 
-file_path = "/app/storage/raw_data/kcc_data_2025_04/1747746883.8694966.37520cbae1.parquet"
+# file path for all raw data parquet files
+file_path = "/app/storage/raw_data/kcc_data_*.parquet"
 
+# read all parquet files as a single dataframe
 df = spark.read.option("header", True).parquet(file_path)
 df.printSchema()
 df.show(truncate=False)
+
+# stage 1 - data cleaning
+# column casting, column names standardising if required
+# filling up Null values with placeholders such they can be grouped later
+# stage 2 - data transformation
+# create star schema: 
+# fact_queries - query_id (surrogate key needs to be generated), state_id, district_id, category_id, crop_id, query_text, query_answer
